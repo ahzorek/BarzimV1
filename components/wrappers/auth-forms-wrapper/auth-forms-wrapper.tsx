1a311f104d170108af9647e00e9e9ec5cf8dc1dc
Andre <zorek@me.com>
2024-07-04 15:32:30 +0000

    :bug: fix: resolve problema de responsividade nos layouts das rotas *auth*

--- components/wrappers/auth-forms-wrapper/auth-forms-wrapper.tsx
+++ components/wrappers/auth-forms-wrapper/auth-forms-wrapper.tsx
@@ -3,7 +3,7 @@ export const AuthFormsWrapper: React.FC<{ children: React.ReactNode }> = ({
 }) => {
   return (
     <div className="overflow-y-auto">
-      <div className="grid w-full grid-cols-[1fr_1fr] items-center gap-6">
+      <div className="grid w-full items-center gap-6 px-2 lg:grid-cols-[1fr_1fr] lg:px-0">
         {children}
       </div>
     </div>
